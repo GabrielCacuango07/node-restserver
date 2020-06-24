0aa78665de6408a91b652d8278415495d573e6b7
GabrielCacuango07 <gabrielcacuango07@gmail.com>
2020-06-24 11:50:05 -0500

    comments

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-//primer archivo lo lee 
+//firts  file to read and execute read the config firts
 require('./config/config');
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 
-
+// capa intermedia que va a estar procesando cosas middelword en este caso procesa todas las peticiones 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
@@ -18,14 +18,13 @@ app.get('/usuario', function (req, res) {
 });
 app.post('/usuario', function (req, res) {
     let body = req.body;
+    //validation of data information send 
     if(body.nombre === undefined){
         res.status(400).json({
             ok:false,
-            mensaje:'el nombre es necesarioa'
+            message:'the name is required for register'
         });
-    }else {
-
-        
+    }else {  
         res.json({persona:body});
     }
 
@@ -39,6 +38,7 @@ app.post('/usuario', function (req, res) {
 app.delete('/usuario', function (req, res) {
     res.json('delete  Usuarios');
   });
+  //the porcess.env.PORT is getting from config.js
 app.listen(process.env.PORT,()=>{
     console.log('escuchando puerto',3000);
 }) 
